532803255953e2c37d8d60f183d45c369724a991
kamaldhankhar <kamaldhankhararts@gmail.com>
2025-02-19 20:53:23 +0530

    Update config.py
    Signed-off-by: kamaldhankhar <kamaldhankhararts@gmail.com>

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("5512860435"))
 
 PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/yhyqeu.jpg")
 START_IMG = getenv("START_IMG", "https://files.catbox.moe/yhyqeu.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+MdUrkBj4zKY4Nzk1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Savage_night_club")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Inflix_Destroyer")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5512860435").split()))
